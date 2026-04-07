9fd95298e829f5728efacaf2b64a7042bc64c210
Darklov0 <shotabolkvadze32@gmail.com>
2026-04-07 14:33:34 +0400

    added !

--- logger.py
+++ logger.py
@@ -15,7 +15,7 @@ class Logger:
         if level not in ["INFO", "DEBUG"]:
             raise ValueError("Level must be INFO or DEBUG")
         if not message or not message.strip():
-            raise ValueError('Message must not be null or empty')
+            raise ValueError('Message must not be null or empty!')
         print(f"[{level}]: {message}")
 
     def check_number(self, number):
