71d1669d9eb6e326563f98d523debda68639153e
Darklov0 <shotabolkvadze32@gmail.com>
2026-04-07 14:30:37 +0400

    Text change

--- logger.py
+++ logger.py
@@ -15,7 +15,7 @@ class Logger:
         if level not in ["INFO", "DEBUG"]:
             raise ValueError("Level must be INFO or DEBUG")
         if not message or not message.strip():
-            raise ValueError('Message must not be null or empty')
+            raise ValueError('Message must not be blank')
         print(f"[{level}]: {message}")
 
 
